4ca4727010f1ec074d3facdb0d4d08b0d963ebb7
MauKMu <maukmu@users.noreply.github.com>
2018-04-20 02:51:19 -0400

    Update LSystem.ts

--- src/l-system/LSystem.ts
+++ src/l-system/LSystem.ts
@@ -1,15 +1,15 @@
 import {vec3, vec4, mat4, quat} from 'gl-matrix';
 import Turtle from './Turtle';
 import {LSymbol, ExpansionRule} from './LSymbol';
-import Plant from '../geometry/Plant';
-import {BRANCH_COLOR} from '../geometry/Plant';
+import Decoration from '../geometry/Decoration';
+import {BRANCH_COLOR} from '../geometry/Decoration';
 import LString from './LString';
 import {lRandom} from './LRandom';
 
 class LSystem {
     alphabet: Array<LSymbol>;
     turtleStack: Array<Turtle>;
-    plant: Plant;
+    decoration: Decoration;
     axiom: Array<LSymbol>;
     lstring: LString;
 
@@ -17,7 +17,7 @@ class LSystem {
         //this.alphabet = [];
         this.initAlphabet();
         this.turtleStack = [new Turtle()];
-        this.plant = new Plant(vec3.fromValues(0, 0, 0));
+        this.decoration = new Decoration();
         this.axiom = [];
         this.lstring = new LString([]);
     }
@@ -32,50 +32,40 @@ class LSystem {
     }
 
     useColor(color: vec4) {
-        this.plant.useColor(color);
+        this.decoration.useColor(color);
     }
 
     addPrismAtTurtle(turtle: Turtle) {
-        if (!this.plant.isSafeToGrow()) {
+        if (!this.decoration.isSafeToGrow()) {
             return;
         }
         let trans = turtle.getTransformationToTurtle();
-        this.plant.addPrism(trans, 8, turtle.scaleBottom, turtle.scaleTop, 1);
+        this.decoration.addPrism(trans, 8, turtle.scaleBottom, turtle.scaleTop, 1);
         turtle.scaleBottom = turtle.scaleTop;
         turtle.scaleTop *= 0.99;
     }
 
-    addTipPrismAtTurtle(turtle: Turtle) {
-        if (!this.plant.isSafeToGrow()) {
-            return;
-        }
-        let trans = turtle.getTransformationToTurtle();
-        this.plant.addPrism(trans, 8, turtle.scaleBottom, turtle.scaleTop * 0.333, 1);
-        turtle.scaleBottom = turtle.scaleTop;
-        turtle.scaleTop *= 0.8;
-    }
-
     addScaledPrismAtTurtle(turtle: Turtle, scaleHeight: number) {
-        if (!this.plant.isSafeToGrow()) {
+        if (!this.decoration.isSafeToGrow()) {
             return;
         }
         let trans = turtle.getTransformationToTurtle();
-        this.plant.addPrism(trans, 8, turtle.scaleBottom, turtle.scaleTop, scaleHeight);
+        this.decoration.addNormalCorrectPrism(trans, 8, turtle.scaleBottom, turtle.scaleTop, scaleHeight);
         turtle.scaleBottom = turtle.scaleTop;
         turtle.scaleTop *= 0.8;
     }
 
     // does not shrink thickness
     addScaledPrismAtTurtleNoShrink(turtle: Turtle, scaleHeight: number) {
-        if (!this.plant.isSafeToGrow()) {
+        if (!this.decoration.isSafeToGrow()) {
             return;
         }
         let trans = turtle.getTransformationToTurtle();
-        this.plant.addPrism(trans, 8, turtle.scaleBottom, turtle.scaleTop, scaleHeight);
+        this.decoration.addNormalCorrectPrism(trans, 8, turtle.scaleBottom, turtle.scaleTop, scaleHeight);
     }
 
     addPearAtTurtle(turtle: Turtle, pearMesh: any) {
-        if (!this.plant.isSafeToGrow()) {
+        if (!this.decoration.isSafeToGrow()) {
             return;
         }
         // refuse to draw overly tiny pears
@@ -101,7 +91,7 @@ class LSystem {
         mat4.fromTranslation(m, vec3.fromValues(0, -1, 0));
         mat4.multiply(toOrigin, toOrigin, m);
         mat4.multiply(trans, trans, toOrigin);
-        this.plant.addDecoration(pearMesh, trans);
+        this.decoration.addDecoration(pearMesh, trans);
     }
 
     initAlphabet() {
@@ -125,23 +115,23 @@ class LSystem {
         this.lstring.execute(this);
     }
 
-    // resets expansions and plant VBOs
+    // resets expansions and decoration VBOs
     resetSystem() {
         this.lstring.fromArray(this.axiom);
-        this.resetPlant();
+        this.resetDecoration();
     }
 
-    resetPlant() {
-        //this.plant.destroy();
-        this.plant.clearBuffers();
-        this.plant.wasSafe = true;
-        this.plant.useColor(BRANCH_COLOR);
+    resetDecoration() {
+        //this.decoration.destroy();
+        this.decoration.clearBuffers();
+        this.decoration.wasSafe = true;
+        this.decoration.useColor(BRANCH_COLOR);
         this.turtleStack = [new Turtle()];
     }
 
-    createPlant() {
+    createDecoration() {
         this.executeString();
-        this.plant.create();
+        this.decoration.create();
     }
 };
 
