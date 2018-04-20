f1add4f96c3b0fa8155679de515067306303197a
MauKMu <maukmu@users.noreply.github.com>
2018-04-20 04:41:34 -0400

    Add Collider class

---
+++ src/game/Collider.ts
@@ -0,0 +1,33 @@
+import Camera from '../Camera';
+import {clamp} from '../Utils';
+import {vec2, vec3, mat4} from 'gl-matrix';
+
+class Collider {
+    position: vec2;
+    radius: number;
+
+    constructor(position: vec2, radius: number) {
+        this.position = vec2.clone(position);
+        this.radius = radius;
+    }
+
+    // if collision happens, returns position where no collision happens
+    // else, return null
+    collide(otherPos: vec2, otherRadius: number): vec2 {
+        let dist = vec2.distance(this.position, otherPos);
+        if (dist >= this.radius + otherRadius) {
+            return null;
+        }
+        else {
+            let toOther = vec2.create();
+            vec2.subtract(toOther, otherPos, this.position);
+            vec2.normalize(toOther, toOther);
+            vec2.scale(toOther, toOther, Math.sqrt(this.radius + otherRadius));
+            vec2.add(toOther, toOther, this.position);
+            return toOther;
+        }
+    }
+
+};
+
+export default Collider;
