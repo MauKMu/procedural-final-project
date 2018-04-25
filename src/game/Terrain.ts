f357339878b876c667a802cff113fedf3d16b223
MauKMu <maukmu@users.noreply.github.com>
2018-04-25 05:36:58 -0400

    Begin terrain for new level

--- src/game/Terrain.ts
+++ src/game/Terrain.ts
@@ -47,8 +47,158 @@ class Terrain {
         this.terrainPlanes = [];
         this.drawables = [];
 
-        this.buildLevel2();
+        this.buildLevel3();
     }
+
+    buildLevel3() {
+        let planeOrigin = vec3.create();
+        let planeOffset = vec3.create();
+        for (let x = 0; x < this.planeNumX; x++) {
+            for (let z = 0; z < this.planeNumZ; z++) {
+                vec3.set(planeOffset, x, 0, z);
+                vec3.scaleAndAdd(planeOrigin, this.origin, planeOffset, this.planeDim);
+                // constructor will generate height fields
+                let heightModifier = function (height: number): number {
+                    return Math.pow(height, 0.25) * 5.0;
+                }
+                let tp = new TerrainPlane(planeOrigin, this.tileDim, this.tileNum, 0.2, heightModifier);
+                tp.setColor(SNOW_COLOR);
+                //tp.create();
+                // planes inactive at first; made active when updated
+                tp.isActive = false;
+                this.terrainPlanes.push(tp);
+            }
+        }
+        // "stitch" height fields at borders together
+        // X border: 0, planeNumX - 1
+        for (let z = 0; z < this.planeNumZ; z++) {
+            let tpA = this.terrainPlanes[this.getAbsIdx(0, z)];
+            let tpB = this.terrainPlanes[this.getAbsIdx(this.planeNumX - 1, z)];
+            // average heights
+            for (let j = 0; j < this.tileNum + 1; j++) {
+                let avgHeight = (tpB.heightField[this.tileNum][j] + tpA.heightField[0][j]) * 0.5;
+                tpB.heightField[this.tileNum][j] = avgHeight;
+                tpA.heightField[0][j] = avgHeight;
+            }
+        }
+        // Z border: 0, planeNumZ - 1
+        for (let x = 0; x < this.planeNumX; x++) {
+            let tpA = this.terrainPlanes[this.getAbsIdx(x, 0)];
+            let tpB = this.terrainPlanes[this.getAbsIdx(x, this.planeNumZ - 1)];
+            // average heights
+            for (let j = 0; j < this.tileNum + 1; j++) {
+                let avgHeight = (tpB.heightField[j][this.tileNum] + tpA.heightField[j][0]) * 0.5;
+                tpB.heightField[j][this.tileNum] = avgHeight;
+                tpA.heightField[j][0] = avgHeight;
+            }
+        }
+
+        // actually create planes' VBOs, now that stitching is done
+        // also copy over to drawables
+        for (let i = 0; i < this.terrainPlanes.length; i++) {
+            this.terrainPlanes[i].create();
+            this.drawables.push(this.terrainPlanes[i]);
+        }
+
+        function addTrees(xClone: number, zClone: number, tp: TerrainPlane) {
+            let numClusters = Math.floor(Math.random() * 3.0 + 1.01);
+            for (let cluster = 0; cluster < numClusters; cluster++) {
+                vec3.scaleAndAdd(planeOrigin, this.origin, planeOffset, this.planeDim);
+                let baseInPlane = vec3.fromValues((Math.random() * 0.65 + 0.15) * this.planeDim, 0, (Math.random() * 0.65 + 0.15) * this.planeDim);
+                planeOrigin[0] += baseInPlane[0];
+                planeOrigin[2] += baseInPlane[2];
+                let treesInCluster = Math.floor(Math.random() * 4.0 + 1.01);
+                let treeOrigin = vec3.create();
+                let angle = Math.random() * Math.PI;
+                let angleIncrement = 2.0 * Math.PI / treesInCluster;
+                let posInPlane = vec3.create();
+                for (let i = 0; i < treesInCluster; i++) {
+                    decorations.useColor(SNOW_COLOR);
+                    angle += angleIncrement;
+                    vec3.set(treeOrigin, Math.cos(angle), 0, Math.sin(angle));
+                    vec3.scaleAndAdd(posInPlane, baseInPlane, treeOrigin, 1.6 * this.tileDim);
+                    vec3.scaleAndAdd(treeOrigin, planeOrigin, treeOrigin, 1.6 * this.tileDim);
+                    // find tile information
+                    let posTileIdx = vec2.create();
+                    let posInTile = modfVec2(vec2.fromValues(posInPlane[0], posInPlane[2]), this.tileDim, posTileIdx);
+                    // find and use height
+                    let height = this.getHeight(posInTile, posTileIdx, tp);
+                    treeOrigin[1] = this.origin[1] + height - 1.0;
+                    //let tree = new BasicTree(decorations);
+                    let tree = new Snowman(decorations, Math.floor(Math.random() * 2048));
+                    tree.initAlphabet();
+                    tree.resetTurtleStack(treeOrigin);
+                    tree.expandString();
+                    tree.expandString();
+                    tree.executeString();
+
+                    // add collider
+                    let collider = new Collider(vec2.fromValues(treeOrigin[0], treeOrigin[2]), 1.0);
+                    let xMin = Math.max(0, posTileIdx[0] - 1);
+                    let xMax = Math.min(this.tileNum - 1, posTileIdx[0] + 1);
+                    let zMin = Math.max(0, posTileIdx[1] - 1);
+                    let zMax = Math.min(this.tileNum - 1, posTileIdx[1] + 1);
+                    for (let tileX = xMin; tileX <= xMax; tileX++) {
+                        for (let tileZ = zMin; tileZ <= zMax; tileZ++) {
+                            // TODO: would be nice to check more precisely if
+                            // collider should go in this tile... but this
+                            // should help filter it a bit anyway
+                            tp.colliders[tileX][tileZ].push(collider);
+                        }
+                    }
+                    //this.colliders.push(new Collider(vec2.fromValues(treeOrigin[0], treeOrigin[2]), 1.0));
+
+                    // add clones to maintain continuity when looping
+                    if (xClone != 0) {
+                        let cloneOrigin = vec3.clone(treeOrigin);
+                        cloneOrigin[0] += xClone;
+                        tree.resetTurtleStack(cloneOrigin);
+                        tree.executeString();
+                    }
+                    if (zClone != 0) {
+                        let cloneOrigin = vec3.clone(treeOrigin);
+                        cloneOrigin[2] += zClone;
+                        tree.resetTurtleStack(cloneOrigin);
+                        tree.executeString();
+                    }
+                    if (xClone != 0 && zClone != 0) {
+                        let cloneOrigin = vec3.clone(treeOrigin);
+                        cloneOrigin[0] += xClone;
+                        cloneOrigin[2] += zClone;
+                        tree.resetTurtleStack(cloneOrigin);
+                        tree.executeString();
+                    }
+                }
+            }
+        }
+
+        // add some decorations
+        let decorations = new Decoration();
+        let decorationMat = mat4.create();
+
+        // pick a plane in which to place the pyramid
+        let pyramidX = 2;
+        let pyramidZ = Math.floor(Math.random() * this.planeNumZ * 0.999);
+        console.log(["pyramid: ", pyramidX, pyramidZ]);
+
+        // add decorations on each terrain plane
+        for (let x = 0; x < this.planeNumX; x++) {
+            let xClone = this.totalDimX * ((x == 0) ? 1 : (x == this.planeNumX - 1) ? -1 : 0);
+            for (let z = 0; z < this.planeNumZ; z++) {
+                let zClone = this.totalDimZ * ((z == 0) ? 1 : (z == this.planeNumZ - 1) ? -1 : 0);
+                // find terrain plane so we can add colliders
+                let tp = this.terrainPlanes[this.getAbsIdx(x, z)];
+                // compute position of decoration
+                vec3.set(planeOffset, x, 0, z);
+                addTrees.call(this, xClone, zClone, tp);
+            }
+        }
+
+
+        decorations.create();
+        this.drawables.push(decorations);
+    }
+
 
     buildLevel2() {
         let planeOrigin = vec3.create();
